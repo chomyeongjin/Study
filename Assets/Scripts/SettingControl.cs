05579039f96983ee60de79ef906239bb8cb1f3c2
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-06-25 23:39:30 +0900

    almostDone

---
+++ Assets/Scripts/SettingControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingControl : MonoBehaviour
+{
+    public GameObject settingView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickRestartBtn() {
+        SceneManager.LoadScene("PlayScene");
+        settingView.SetActive(false);
+    }
+
+    public void OnClickSetting() {
+        Time.timeScale = 0;
+        settingView.SetActive(true);
+    }
+}
